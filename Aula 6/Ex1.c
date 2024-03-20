247f11bbd691b5d06506642d3e7df99d7aaa4886
diogotavc <diogotavc@protonmail.com>
2024-03-20 10:10:45 +0000

    Aula 6: Add Exercise 1 and 2

---
+++ Aula 6/Ex1.c
@@ -0,0 +1,22 @@
+#include <detpic32.h>
+
+int main(void)
+{
+    TRISBbits.TRISB4 = 1;    // Disable digital output
+    AD1PCFGbits.PCFG4 = 0;   // Configure as analog input (AN4)
+    AD1CON1bits.SSRC = 7;    // Conversion trigger selection bits
+    AD1CON1bits.CLRASAM = 1; // Stop conv. when 1st adc interrupt is generated
+    AD1CON3bits.SAMC = 16;   // Sample time is 16 TAD (*100 ns)
+    AD1CON2bits.SMPI = 3;    // (4) samples stores in ADC1BUF[0,3]
+    AD1CHSbits.CH0SA = 4;    // Analog channel 4 (AN4)
+    AD1CON1bits.ON = 1;      // Enable adc
+    while (1)
+    {
+        AD1CON1bits.ASAM = 1;             // Start conversion
+        while (IFS1bits.AD1IF == 0);      // Wait while conversion not done (AD1IF == 0)
+        printInt(ADC1BUF0, 16 | 3 << 16); // Read conversion result (ADC1BUF0 value) and print it
+        putChar('\r');                    // Put \r to avoid making the output spammy and unreadable
+        IFS1bits.AD1IF = 0;               // Reset AD1IF
+    }
+    return 0;
+}
